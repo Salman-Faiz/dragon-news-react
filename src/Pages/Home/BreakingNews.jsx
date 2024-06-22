63165f65731d6adddfc99487e106e8ed78664862
Salman-Faiz <salmanfaiz.cse@gmail.com>
2024-06-22 13:35:13 +0600

    nav bar ,profile login btn added

--- src/Pages/Home/BreakingNews.jsx
+++ src/Pages/Home/BreakingNews.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 const BreakingNews = () => {
     return (
-        <div className="flex">
+        <div className="flex bg-slate-100 rounded-lg py-4 my-4">
             <button className="btn btn-secondary">Latest</button>
             <Marquee pauseOnHover={true} speed={200} >
                 <p className="ps-10">I can be a React component, multiple React components, or just <Link className="text-red-500">some text</Link>.</p>
